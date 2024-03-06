024c2ae33274f47b999636ca860af242aa7a2ac9
Anish Kumar <kumaranish200a@gmail.com>
2024-03-06 18:29:46 +0530

    Logo updated

--- ReactRouter/src/components/Footer/Footer.jsx
+++ ReactRouter/src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
           <div className="mb-6 md:mb-0">
             <Link to="/" className="flex items-center">
               <img
-                src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+                src="https://logos.textgiraffe.com/logos/logo-name/Anish-designstyle-boots-m.png"
                 className="mr-3 h-16"
                 alt="Logo"
               />
--- ReactRouter/src/components/header/Header.jsx
+++ ReactRouter/src/components/header/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <Link to="/" className="flex items-center">
             <img
-              src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+              src="https://logos.textgiraffe.com/logos/logo-name/Anish-designstyle-boots-m.png"
               className="mr-3 h-12"
               alt="Logo"
             />
