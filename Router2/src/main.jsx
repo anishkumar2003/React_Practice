24bd735a9d53e124ceaa27140ecf7e88458d8499
Anish Kumar <kumaranish200a@gmail.com>
2024-03-12 16:52:38 +0530

    Routes created

--- Router2/src/main.jsx
+++ Router2/src/main.jsx
@@ -6,21 +6,19 @@ import "./index.css";
 import {
   Route,
   RouterProvider,
-  Routes,
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
 import Home from "./Components/Home.jsx";
 
 const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<App />} >
-        <Route path="home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        
-      </Route>
-    )
-  );
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
