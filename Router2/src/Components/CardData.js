e4c99bf6848ae25c01cdc0ee52933103098de7dc
Anish Kumar <kumaranish200a@gmail.com>
2024-03-12 16:51:48 +0530

    card data using js

---
+++ Router2/src/Components/CardData.js
@@ -0,0 +1,22 @@
+const cdata = [
+  {
+    title: "Card 1",
+    description: "This is the first card",
+    imageUrl:
+      "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29tcHV0ZXJ8ZW58MHx8MHx8fDA%3D",
+  },
+  {
+    title: "Card 2",
+    description: "This is the second card",
+    imageUrl:
+      "https://images.unsplash.com/photo-1568801838106-43cc2000b704?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fG1vbnRhaW58ZW58MHx8MHx8fDA%3D",
+  },
+  {
+    title: "Card 3",
+    description: "This is the third card",
+    imageUrl:
+      "https://images.unsplash.com/photo-1603382585507-45205571d760?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fG1vbnRhaW58ZW58MHx8MHx8fDA%3D",
+  },
+];
+
+export default cdata;
