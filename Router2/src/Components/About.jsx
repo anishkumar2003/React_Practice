0cdb5ae28cbe39c8e112899c9276cfdb2f66fddd
Anish Kumar <kumaranish200a@gmail.com>
2024-03-12 16:50:55 +0530

    style added

--- Router2/src/Components/About.jsx
+++ Router2/src/Components/About.jsx
@@ -1,9 +1,9 @@
 export default function About() {
   return (
-    <div>
-      <div className="container mx-auto px-4 lg:px-6 py-12">
-        <h1>Heading</h1>
-        <p>
+    <div className="flex flex-col justify-center h-screen bg-gray-100">
+      <div className="container mx-auto px-4 lg:px-6 py-12 bg-white rounded shadow">
+        <h1 className="text-4xl font-bold text-center mb-4">Heading</h1>
+        <p className="text-lg text-gray-700">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae esse
           sint laudantium molestias cum, id alias voluptas similique dolorem
           facilis illum aperiam consequuntur perferendis provident sit est
