5f612c833c56ad96fda8029135e1301c1a23dcfa
Anish Kumar <kumaranish200a@gmail.com>
2024-03-13 21:43:39 +0530

    Card created

--- ThemeSwitcher/src/componenets/Card.jsx
+++ ThemeSwitcher/src/componenets/Card.jsx
@@ -4,7 +4,7 @@ export default function Card() {
       <a href="/">
         <img
           className="p-8 rounded-t-lg"
-          src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          src="https://images.unsplash.com/photo-1579586337278-3befd40fd17a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXBwbGUlMjB3YXRjaHxlbnwwfHwwfHx8MA%3D%3D"
           alt="product_image1"
         />
       </a>
